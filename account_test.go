7ceb2bbb03000b838640da9844109ed51e590b75
Isabella Muerte <63051+slurps-mad-rips@users.noreply.github.com>
2021-09-19 12:09:12 -0700

    ✅ Increate account test coverage (#16)
    The errors are basically useless without a sandbox API, but we can at
    least get error coverage
    
    Signed-off-by: Isabella Muerte <63051+slurps-mad-rips@users.noreply.github.com>

--- account_test.go
+++ account_test.go
@@ -3,12 +3,19 @@ package improvmx
 import (
 	"context"
 	_ "embed"
+	"fmt"
+	"net/http"
 	"testing"
 
 	"occult.work/doze/test"
 )
 
 type AccountTestSuite struct {
+	test.Suite
+	session *Session
+}
+
+type AccountErrorTestSuite struct {
 	test.Suite
 	session *Session
 }
@@ -23,8 +30,24 @@ func (suite *AccountTestSuite) SetupSuite() {
 	suite.session, _ = New("account-test-suite", WithBaseURL(suite.Server.URL))
 }
 
+func (suite *AccountErrorTestSuite) SetupSuite() {
+	router := test.NewRouter().
+		Get(accountLabelsPath, func(writer http.ResponseWriter, request *http.Request) {
+			writer.WriteHeader(420)
+			fmt.Fprintf(writer, `{ "error": "fake error", "code": 420, "success": false }`)
+		}).
+		Get(accountReadPath, func(writer http.ResponseWriter, request *http.Request) {
+			writer.WriteHeader(420)
+			fmt.Fprintf(writer, `{ "error": "fake error", "code": 420, "success": false }`)
+		})
+	suite.Initialize(router)
+	suite.Data = &testData
+	suite.session, _ = New("account-error-test-suite", WithBaseURL(suite.Server.URL))
+}
+
 func TestAccount(t *testing.T) {
 	test.Run(t, new(AccountTestSuite))
+	test.Run(t, new(AccountErrorTestSuite))
 }
 
 func (suite *AccountTestSuite) TestRead() {
@@ -40,4 +63,16 @@ func (suite *AccountTestSuite) TestLabels() {
 	suite.Require().NoError(error)
 	suite.Require().NotEmpty(labels)
 	suite.Equal(labels[0].Name, "example.com")
+}
+
+func (suite *AccountErrorTestSuite) TestRead() {
+	account, error := suite.session.Account.Read(context.Background())
+	suite.Require().Error(error)
+	suite.Require().Empty(account)
+}
+
+func (suite *AccountErrorTestSuite) TestLabels() {
+	labels, error := suite.session.Account.Labels(context.Background())
+	suite.Require().Error(error)
+	suite.Require().Empty(labels)
 }
